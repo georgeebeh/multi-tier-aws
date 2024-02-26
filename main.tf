ed8cd80a2acbecdbd32eeeb62d341af996e85dd5
George Ebeh <georgeebeh@gmail.com>
2024-02-26 15:58:38 +0000

    add public route table

--- main.tf
+++ main.tf
@@ -17,7 +17,28 @@ resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create a subnet
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev-public-rt" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Create a private subnet
 resource "aws_subnet" "priv-subnet-1" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.1.0/24"
@@ -27,7 +48,7 @@ resource "aws_subnet" "priv-subnet-1" {
   }
 }
 
-# Create a subnet
+# Create a second private subnet 
 resource "aws_subnet" "priv-subnet-2" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.2.0/24"
@@ -37,7 +58,7 @@ resource "aws_subnet" "priv-subnet-2" {
   }
 }
 
-# Create a subnet
+# Create a public subnet
 resource "aws_subnet" "pub-subnet-1" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.3.0/24"
@@ -47,7 +68,7 @@ resource "aws_subnet" "pub-subnet-1" {
   }
 }
 
-# Create a subnet
+# Create a second public subnet
 resource "aws_subnet" "pub-subnet-2" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.4.0/24"
