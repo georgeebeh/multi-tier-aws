d114637d9c697bd0098d0f2204683905d18d4ac1
George Ebeh <georgeebeh@gmail.com>
2024-02-26 18:01:45 +0000

    remove default values from backend config

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "The AWS region to deploy resources"
-  default     = "eu-west-2"
+  
   
 }
 
 variable "bucketname" {
   description = "The name of the S3 bucket to store Terraform state"
-  default     = "multi-tier-tf-state-bucket-ebeh"
+  
   
 }
