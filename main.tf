3a7a09f0f8b2465b7394c98418ed5edf91267cc5
George Ebeh <georgeebeh@gmail.com>
2024-02-26 17:24:01 +0000

    updated terraform action version to v3

--- main.tf
+++ main.tf
@@ -9,12 +9,14 @@ terraform {
 
 # Configure the AWS Provider  
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-2"
 }
 
 # Create a VPC
 resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "dev-igw" {
@@ -38,10 +40,24 @@ resource "aws_route_table" "dev-public-rt" {
   }
 }
 
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_subnet_association_1" {
+  subnet_id      = aws_subnet.pub-subnet-1.id
+  route_table_id = aws_route_table.dev-public-rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_2" {
+  subnet_id      = aws_subnet.pub-subnet-2.id
+  route_table_id = aws_route_table.dev-public-rt.id
+}
+
+
+
 # Create a private subnet
 resource "aws_subnet" "priv-subnet-1" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"
 
   tags = {
     Name = "priv-subnet-1"
@@ -52,6 +68,7 @@ resource "aws_subnet" "priv-subnet-1" {
 resource "aws_subnet" "priv-subnet-2" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-2b"
 
   tags = {
     Name = "priv-subnet-2"
@@ -62,6 +79,7 @@ resource "aws_subnet" "priv-subnet-2" {
 resource "aws_subnet" "pub-subnet-1" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "eu-west-2a"
 
   tags = {
     Name = "pub-subnet-1"
@@ -72,8 +90,22 @@ resource "aws_subnet" "pub-subnet-1" {
 resource "aws_subnet" "pub-subnet-2" {
   vpc_id     = aws_vpc.dev-vpc.id
   cidr_block = "10.0.4.0/24"
+  availability_zone = "eu-west-2b"
 
   tags = {
     Name = "pub-subnet-2"
   }
+}
+
+# Database Subnets
+resource "aws_subnet" "db_subnet_1" {
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_subnet" "db_subnet_2" {
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "eu-west-2a"
 }
