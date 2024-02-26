80a140b57c4c6f89ceb5bf0ce20ee5d2d652cf9b
George Ebeh <georgeebeh@gmail.com>
2024-02-26 17:59:30 +0000

    add s3 backend configuration

---
+++ terraform/backend/s3-backend.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.region
+}
+
+# Create an S3 bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state_s3" {
+  bucket = var.bucketname
+}
+
+resource "aws_s3_bucket_acl" "s3-backend-acl" {
+  bucket = aws_s3_bucket.terraform_state_s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3-backend-versioning" {
+  bucket = aws_s3_bucket.terraform_state_s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+# Create a DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
