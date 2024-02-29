a9974e73b0cab9d83a4365c780c55038a24fe7eb
George Ebeh <georgeebeh@gmail.com>
2024-02-29 15:31:42 +0000

    modified the asg module

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -1,12 +1,24 @@
+resource "aws_key_pair" "my-key" {
+  key_name   = "my-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUGNwuwQV9gPI0TqO2YUOPInDBTXv3S+6jaL4T7Fbo+ georgeebeh@gmail.com"
+}
+
+
 resource "aws_security_group" "sg" {
   name   = "${var.asg_name}_sg"
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [var.alb_sg_id]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [var.alb_sg_id]
   }
 
   egress {
@@ -15,6 +27,21 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 data "template_file" "user_data" {
@@ -31,7 +58,7 @@ data "template_file" "user_data" {
 
 resource "aws_launch_template" "lt" {
   name          = "${var.asg_name}_lt"
-  image_id      = var.image_id
+  image_id      = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data = base64encode(data.template_file.user_data.rendered)
@@ -48,9 +75,25 @@ resource "aws_autoscaling_group" "asg" {
     id      = aws_launch_template.lt.id
     version = "$Latest"
   }
-  vpc_zone_identifier  = var.private_subnet_ids
+  #availability_zones = ["us-east-1a", "us-east-1b"]
+  vpc_zone_identifier  = [ var.priv-subnet-1-id, var.priv-subnet-2-id ]
+  target_group_arns =  [ var.alb_target_group_arn]
   min_size             = var.min_size
   max_size             = var.max_size
   desired_capacity     = var.desired_capacity
   health_check_type    = "EC2"
+
+  tag {
+    key                 = "Key"
+    value               = "Value"
+    propagate_at_launch = true
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
 }
--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,6 +1,7 @@
 variable "asg_name" {
   description = "Name for the Auto Scaling Group"
   type        = string
+  default = "dev-asg"
 }
 
 variable "image_id" {
@@ -23,11 +24,15 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "private_subnet_ids" {
+variable "priv-subnet-1-id" {
   description = "A list of subnet IDs to launch resources in"
-  type        = list(string)
-  default = [ "value" ]
-}
+  type        = string
+  }
+variable "priv-subnet-2-id" {
+  description = "A list of subnet IDs to launch resources in"
+  type        = string
+  }
+
 
 variable "min_size" {
   description = "Minimum size of the Auto Scaling Group"
@@ -42,4 +47,14 @@ variable "max_size" {
 variable "desired_capacity" {
   description = "Desired number of instances in the Auto Scaling Group"
   type        = number
+}
+
+variable "alb_sg_id" {
+  description = "The ID of the ALB security group"
+  type        = string
+}
+
+variable "alb_target_group_arn" {
+  description = "The arn of the ALB target group"
+  type        = string
 }
